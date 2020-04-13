a2df791f0c9a2d57ab601bbf63041696f8f824b4
Aznavour <vic00tor@gmail.com>
2020-04-13 17:41:00 -0400

    ajout du premier test d'importation de média

--- js/infoProjet.js
+++ js/infoProjet.js
@@ -3,18 +3,37 @@ window.onload = function(){
 
     //au moment de la création de la page, ajout des events listener pour les projets
     let aoProjets = document.querySelectorAll(".desc");
-   
+    var createImage = function(src, title) { 
+        var img = new Image(); 
+        img.src = src; 
+        img.alt = title; 
+        img.title = title; 
+        return img; 
+      }; 
+      // array of images 
+        var images = []; 
+        var sSrc = ['captureAffiche.jpg','captureJeu.jpg','captureJeuTitre.jpg','captureArcade.jpg','captureArcadeWireframe.jpg','captureArcadeJeu.jpg','theLonelySniper.jpg','compositionPersonnage.jpg','compositionPersonnage2.jpg','portfolio-6.jpg','portfolio-3.jpg','portfolio-5.jpg'];
+      // push two images to the array 
+      for(let i = 0;i<sSrc.length;i++){
+        images[i] = createImage('../images/' + sSrc[i], "image"+i); 
+      }
     for(let i = 0;i<aoProjets.length;i++){
         aoProjets[i].addEventListener("click", function(){
-            test(aoProjets[i]);
+            test(aoProjets[i],images);
         });
     }
 }
 
 //fonction qui va créer l'objet du projet
-function test(objetProjet){
+function test(objetProjet,images){
     console.log(objetProjet.id);
-    var animProjets = new animProjet(0,0,objetProjet.id);
+    
+       
+       
+      
+      // output 
+        console.log(images);
+    var animProjets = new animProjet(0,0,objetProjet.id,images);
     let oRectangle3 = document.querySelector('.rectangleProjet3');
     console.log(oRectangle3);
 
